a0e3bfb72e89f6abb94b697c7795b5dd8f54fb4b
Jordan <JDLChang@gmail.com>
2022-09-24 19:39:14 -0500

    adds counter javascript on form

--- actions.js
+++ actions.js
@@ -5,3 +5,51 @@ function openForm(id) {
   function closeForm(id) {
     document.getElementById(id).style.display = "none";
   }
+
+/* button for plus and minus AND */
+  window.onload=function(){
+    var minusBtn = document.getElementById("minus"),
+        plusBtn = document.getElementById("plus"),
+        numberPlace = document.getElementById("numberPlace"),
+        submitBtn = document.getElementById("submit"),
+        number = 0, /// number value
+        min = 0, /// min number
+        max = 30; /// max number
+        
+    minusBtn.onclick = function(){
+        if (number>min){
+           number = number-1; /// Minus 1 of the number
+           numberPlace.innerText = number ; /// Display the value in place of the number
+           
+        }
+        if(number == min) {        
+            numberPlace.style.color= "red";
+            setTimeout(function(){numberPlace.style.color= "black"},500)
+        }
+        else {
+          numberPlace.style.color="black";            
+           }
+                
+    }
+    plusBtn.onclick = function(){
+        if(number<max){
+           number = number+1;
+           numberPlace.innerText = number ; /// Display the value in place of the number
+        }     
+        if(number == max){
+               numberPlace.style.color= "red";
+               setTimeout(function(){numberPlace.style.color= "black"},500)
+        }
+           
+        else {
+               numberPlace.style.color= "black";
+               
+        }
+     
+           
+    }
+    submitBtn.onclick = function(){
+        alert("you choice : " + number);
+    }
+    
+}
